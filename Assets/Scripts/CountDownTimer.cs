d13f2c889eae3cb2a78cc9b0e51cf1cd0fa734b9
stoldalc <christianstoldal@gmail.com>
2020-04-21 13:46:02 -0700

    Added timer and game over scene when timer runs out you are taken to the game over screen

---
+++ Assets/Scripts/CountDownTimer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class CountDownTimer : MonoBehaviour
+{
+
+    public float startTime = 10;
+    public float currentTime = 0;
+
+    [SerializeField] Text countDownText;
+
+    public string GameOverScene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime = startTime;    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime -= 1 * Time.deltaTime;
+        countDownText.text = currentTime.ToString();
+        print(currentTime);
+        if(currentTime <= 1)
+        {
+            SceneManager.LoadScene(GameOverScene);
+        }
+    }
+}
